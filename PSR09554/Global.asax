83597ee0b9703ef706d61c29425e373f0a1bf37e
Jeremy PROOT <PSR09554@students.ephec.be>
2019-11-24 23:17:22 +0100

    Added new stuff

---
+++ PSR09554/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PSR09554.MvcApplication" Language="C#" %>
---
+++ PSR09554/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PSR09554.Startup))]
+namespace PSR09554
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
